7f11b51dc20df16341e397020da93cf9d51a7f63
mchaveez <martin.charvee@gmail.com>
2024-07-17 07:20:24 -0500

    remove order route
    Co-authored-by: scottliuu <scott.liu623@gmail.com>

--- server/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const orderController = require("../controllers/orderController");
-const router = express.Router();
-
-router.post("/add", orderController.addOrder);
-
-module.exports = router;
